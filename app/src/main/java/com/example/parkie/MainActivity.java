a01b5738636e2ff1cd86b76586ef16995ec28d25
AdeelAhmed3 <38222370+AdeelAhmed3@users.noreply.github.com>
2021-07-04 03:31:31 +0500

    change 1

--- app/src/main/java/com/example/parkie/MainActivity.java
+++ app/src/main/java/com/example/parkie/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.parkie;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputLayout;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    TextInputLayout emailText,passwordText;
+    Button signInBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emailText = findViewById(R.id.email_text);
+        passwordText = findViewById(R.id.password_text);
+        signInBtn = findViewById(R.id.signinBtn);
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/parkie/ScannedBarcodeActivity.java
@@ -0,0 +1,34 @@
+package com.example.parkie;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import java.io.IOException;
+
+public class ScannedBarcodeActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanned_barcode);
+
+
+    }
+
+}
