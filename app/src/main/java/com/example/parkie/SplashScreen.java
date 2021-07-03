2883f61b3650304350e576c40e46b3cf84e29f6d
AdeelAhmed3 <38222370+AdeelAhmed3@users.noreply.github.com>
2021-07-04 04:07:05 +0500

    change 2

--- app/src/main/java/com/example/parkie/SplashScreen.java
+++ app/src/main/java/com/example/parkie/SplashScreen.java
@@ -2,13 +2,35 @@ package com.example.parkie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
-
+    Animation carMove;
+    ImageView logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        carMove = AnimationUtils.loadAnimation(SplashScreen.this,R.anim.car_move);
+        logo = findViewById(R.id.logo_splash);
+        logo.startAnimation(carMove);
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },1500);
+
     }
 }
